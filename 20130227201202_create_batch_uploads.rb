b35afc2b0d385a069ecf716b03b533ca33527d66
Eben English <ebenenglish@users.noreply.github.com>
2015-06-25 09:52:48 -0400

    need to keep CreateBatchUploads class or db:migrate throws an error

--- 20130227201202_create_batch_uploads.rb
+++ 20130227201202_create_batch_uploads.rb
@@ -1,18 +1,17 @@
 # -*- encoding : utf-8 -*-
-=begin
 # commenting out as this is throwing errors
 # 'attachments' may be reserved word in Rails 4 maybe?
 class CreateBatchUploads < ActiveRecord::Migration
+
   def self.up
-    create_table :batch_uploads do |t|
-      t.attachment :upload
-      t.timestamps
-    end
+    #create_table :batch_uploads do |t|
+    #  t.attachment :upload
+    #  t.timestamps
+    #end
   end
 
   def self.down
-    drop_table :batch_uploads
+    #drop_table :batch_uploads
   end
-
+  
 end
-=end
